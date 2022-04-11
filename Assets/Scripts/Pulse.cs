fe4d7cb8503718056deb55b9a42e71fdd68e4cca
Ronald DeSilva <61198729+RonaldDeSilva@users.noreply.github.com>
2022-04-11 11:35:28 -0400

    Tried implementing haptic feedback

--- Assets/Scripts/Pulse.cs
+++ Assets/Scripts/Pulse.cs
@@ -1,41 +1,45 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
+
 
 public class Pulse : MonoBehaviour
 {
-    //I will attempt to use the vibrating motors in the oculus quest controllers
-    //None of this works because XRController isn't a thing :<
-    /*
-    private XRController xr;
-
-    void Start()
-    {
-        //xr = (XRController)GameObject.FindObjectOfType(typeof(XRController));
-        xr = GetComponent<XRController>();
-        xr.SendHapticImpulse(0.7f, 2f);
-    }
-
-
+    public XRController xr1;
+    public XRController xr2;
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Hand"))
+        if (other.CompareTag("Hand1"))
         {
-            StartCoroutine("Heartbeat");
+            StartCoroutine("HeartbeatR");
+        }else if (other.CompareTag("Hand2"))
+        {
+            StartCoroutine("HeartbeatL");
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Hand"))
+        if (other.CompareTag("Hand1") || other.CompareTag("Hand2"))
         {
-            StopCoroutine("Heartbeat");
+            StopAllCoroutines();
         }
+
     }
 
-    IEnumerator Heartbeat()
+    IEnumerator HeartbeatR()
     {
+        xr1.SendHapticImpulse(0.5f, 0.1f);
         yield return new WaitForSeconds(1f);
     }
-    */
+
+    IEnumerator HeartbeatL()
+    {
+        //xr.gameObject.GetComponentInParent<XRController>().SendHapticImpulse(0.7f, 2f);
+        yield return new WaitForSeconds(1f);
+    }
 }
